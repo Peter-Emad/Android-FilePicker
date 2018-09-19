9d8f908e61032abbea984ee7192b955c1bde3185
OTVENTURES\Peter.Helmy <Peter.helmy@linkdev.com>
2018-09-19 13:25:32 +0200

    adding video capture feature

--- filepicker/src/main/java/droidninja/filepicker/utils/ImageCaptureManager.java
+++ filepicker/src/main/java/droidninja/filepicker/utils/ImageCaptureManager.java
@@ -7,109 +7,147 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.provider.Settings;
 import android.support.v4.content.FileProvider;
 import android.text.TextUtils;
 import android.util.Log;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
+import droidninja.filepicker.FilePickerConst;
 import droidninja.filepicker.PickerManager;
 
 public class ImageCaptureManager {
 
-  private final static String CAPTURED_PHOTO_PATH_KEY = "mCurrentPhotoPath";
-  public static final int REQUEST_TAKE_PHOTO = 0x101;
+    private final static String CAPTURED_PHOTO_PATH_KEY = "mCurrentPhotoPath";
+    public static final int REQUEST_CAPTURE_MEDIA = 0x101;
 
-  private String mCurrentPhotoPath;
-  private Context mContext;
+    private String mCurrentMediaFilePath;
+    private Context mContext;
 
-  public ImageCaptureManager(Context mContext) {
-    this.mContext = mContext;
-  }
+    public ImageCaptureManager(Context mContext) {
+        this.mContext = mContext;
+    }
 
-  private File createImageFile() throws IOException {
-    // Create an image file name
+    private File createImageFile() throws IOException {
+        // Create an image file name
 //    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(new Date());
-    String imageFileName = "JPEG_" + System.currentTimeMillis() + ".jpg";
-    File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String imageFileName = "JPEG_" + System.currentTimeMillis() + ".jpg";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
 
-    if (!storageDir.exists()) {
-      if (!storageDir.mkdir()) {
-        Log.e("TAG", "Throwing Errors....");
-        throw new IOException();
-      }
+        if (!storageDir.exists()) {
+            if (!storageDir.mkdir()) {
+                Log.e("TAG", "Throwing Errors....");
+                throw new IOException();
+            }
+        }
+
+        File image = new File(storageDir, imageFileName);
+        //                File.createTempFile(
+        //                imageFileName,  /* prefix */
+        //                ".jpg",         /* suffix */
+        //                storageDir      /* directory */
+        //        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentMediaFilePath = image.getAbsolutePath();
+        return image;
     }
 
-    File image = new File(storageDir, imageFileName);
-    //                File.createTempFile(
-    //                imageFileName,  /* prefix */
-    //                ".jpg",         /* suffix */
-    //                storageDir      /* directory */
-    //        );
 
-    // Save a file: path for use with ACTION_VIEW intents
-    mCurrentPhotoPath = image.getAbsolutePath();
-    return image;
-  }
+    private File createVideoFile() throws IOException {
+        // Create an image file name
+//    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(new Date());
+        String videoFileName = "MP4_" + System.currentTimeMillis() + ".mp4";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
 
+        if (!storageDir.exists()) {
+            if (!storageDir.mkdir()) {
+                Log.e("TAG", "Throwing Errors....");
+                throw new IOException();
+            }
+        }
 
-  public Intent dispatchTakePictureIntent(Context context) throws IOException {
-    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-// Ensure that there's a camera activity to handle the intent
-    if (takePictureIntent.resolveActivity(mContext.getPackageManager()) != null) {
-// Create the File where the photo should go
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        File newFile = createImageFile();
-        takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-        Uri photoURI = FileProvider.getUriForFile(context, PickerManager.getInstance().getProviderAuthorities(), newFile);
-        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-      } else {
-        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(createImageFile()));
-      }
-      return takePictureIntent;
-    }
-    return null;
-  }
+        File video = new File(storageDir, videoFileName);
+        //                File.createTempFile(
+        //                imageFileName,  /* prefix */
+        //                ".jpg",         /* suffix */
+        //                storageDir      /* directory */
+        //        );
 
-
-  public String notifyMediaStoreDatabase() {
-    Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-
-    if (TextUtils.isEmpty(mCurrentPhotoPath)) {
-      return null;
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentMediaFilePath = video.getAbsolutePath();
+        return video;
     }
 
-    File f = new File(mCurrentPhotoPath);
-    Uri contentUri = Uri.fromFile(f);
-    mediaScanIntent.setData(contentUri);
-    mContext.sendBroadcast(mediaScanIntent);
 
-    return mCurrentPhotoPath;
-  }
+    public Intent dispatchTakePictureIntent(Context context, int fileType) throws IOException {
+        Intent cameraIntent = new Intent();
 
+        if (fileType == FilePickerConst.MEDIA_TYPE_IMAGE)
+            cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        else if (fileType == FilePickerConst.MEDIA_TYPE_VIDEO)
+            cameraIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
 
-  public String getCurrentPhotoPath() {
-    return mCurrentPhotoPath;
-  }
+        //Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (cameraIntent.resolveActivity(mContext.getPackageManager()) != null) {
 
+            // Create the File where the photo should go
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
 
-  public void onSaveInstanceState(Bundle savedInstanceState) {
-    if (savedInstanceState != null && mCurrentPhotoPath != null) {
-      savedInstanceState.putString(CAPTURED_PHOTO_PATH_KEY, mCurrentPhotoPath);
+                File file = new File("");
+                if (fileType == FilePickerConst.MEDIA_TYPE_IMAGE)
+                    file = createImageFile();
+                else if (fileType == FilePickerConst.MEDIA_TYPE_VIDEO)
+                    file = createVideoFile();
+
+                cameraIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                cameraIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+                Uri mediaFileURI = FileProvider.getUriForFile(context, PickerManager.getInstance().getProviderAuthorities(), file);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mediaFileURI);
+
+            } else {
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(createImageFile()));
+            }
+            return cameraIntent;
+        }
+        return null;
     }
-  }
 
-  public void onRestoreInstanceState(Bundle savedInstanceState) {
-    if (savedInstanceState != null && savedInstanceState.containsKey(CAPTURED_PHOTO_PATH_KEY)) {
-      mCurrentPhotoPath = savedInstanceState.getString(CAPTURED_PHOTO_PATH_KEY);
+
+    public String notifyMediaStoreDatabase() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+
+        if (TextUtils.isEmpty(mCurrentMediaFilePath)) {
+            return null;
+        }
+
+        File f = new File(mCurrentMediaFilePath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        mContext.sendBroadcast(mediaScanIntent);
+
+        return mCurrentMediaFilePath;
     }
-  }
+
+
+    public String getCurrentMediaFilePathPath() {
+        return mCurrentMediaFilePath;
+    }
+
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState != null && mCurrentMediaFilePath != null) {
+            savedInstanceState.putString(CAPTURED_PHOTO_PATH_KEY, mCurrentMediaFilePath);
+        }
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState != null && savedInstanceState.containsKey(CAPTURED_PHOTO_PATH_KEY)) {
+            mCurrentMediaFilePath = savedInstanceState.getString(CAPTURED_PHOTO_PATH_KEY);
+        }
+    }
 
 }
